8a2c6c75a86358f873026cbb94294e4f5695ce91
Jacky Wang <zwang@flexport.com>
2022-01-13 18:44:43 +0800

    initial projects

---
+++ test.js
@@ -0,0 +1,6 @@
+var bgContainer = document.querySelector('.is-dynamic-background');
+var showGreeting = () => bgContainer.className = "zx-dynamic-background is-greeing";
+var showProgram = () => bgContainer.className = "zx-dynamic-background is-program";
+var showPhotoGraph = () => bgContainer.className = "zx-dynamic-background is-photograph";
+var showDrawing = () => bgContainer.className = "zx-dynamic-background is-drawing";
+var showContact = () => bgContainer.className = "zx-dynamic-background is-contact";
